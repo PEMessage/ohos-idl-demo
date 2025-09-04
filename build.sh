3880b8405428e7752def32cbb9a5936da6b199c6
PEMessage <1165739182@qq.com>
2025-09-04 10:32:37 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# See: https://blog.csdn.net/qq_37793601/article/details/127484310
+#      https://github.com/openharmony/docs/blob/40ff3d68bbca2c6c83a8b35a47bf932a2ef27a63/zh-cn/application-dev/IDL/idl-guidelines.md?plain=1#L159
+
+set -xe
+
+
+idl_tool() {
+    mkdir -p foundation/ability/idl_tool
+    git -C foundation/ability/idl_tool \
+        clone https://gitee.com/openharmony/ability_idl_tool.git \
+        -b OpenHarmony-v5.1.0-Release . || echo "ignore error"
+}
+
+bounds_checking_function() {
+    mkdir -p third_party/bounds_checking_function
+    git -C third_party/bounds_checking_function \
+        clone https://gitee.com/openharmony/third_party_bounds_checking_function.git \
+        -b OpenHarmony-v5.1.0-Release . || echo "ignore error"
+}
+
+
+idl_tool
+bounds_checking_function
+make -C foundation/ability/idl_tool
+if [ ! -f idl-gen ] ; then
+    ln -s foundation/ability/idl_tool .
+    ln -s foundation/ability/idl_tool/idl-gen . 
+fi
